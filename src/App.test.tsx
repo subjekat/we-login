1e0f508e29100eed0089247b46f5b88f2ea5b3d0
Almir Dzananovic <116034331+subjekat@users.noreply.github.com>
2022-12-22 23:43:33 +0100

    test github.dev

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
+//test github.dev
+
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
